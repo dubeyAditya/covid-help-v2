7333e010d575a2eafce7a6c7f287d7f568dc478a
Aditya Dubey <sinister.dubey@gmail.com>
2021-04-30 00:52:17 +0530

    added Usecallback

--- src/component/AppContainer/DashBoard/Filters/Filter.jsx
+++ src/component/AppContainer/DashBoard/Filters/Filter.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useCallback } from "react";
 import { Select } from "antd";
 import { appContext } from "../../../../context";
 const { Option } = Select;
@@ -7,9 +7,9 @@ const Filter = () => {
   const { resources, setFilter } = useContext(appContext);
   const [locations, setLoactions] = useState({});
 
-  function handleChange(value) {
+   const handleChange = useCallback((value) => {
     setFilter(value);
-  }
+   });
 
   useEffect(() => {
     setLoactions(() => {
