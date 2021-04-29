d76669eb1e1e9fc857950c767884551c61059edf
Prabhat Gupta <pgupta@collegeboard.org>
2021-04-29 15:34:13 +0530

    stiky header

--- src/component/AppContainer/DashBoard/DashBoard.jsx
+++ src/component/AppContainer/DashBoard/DashBoard.jsx
@@ -11,30 +11,28 @@ import ResourceList from "./ResourceList";
 const { Content, Sider } = Layout;
 
 const DashBoard = () => {
-
   const [container, setContainer] = useState(null);
   return (
-    <Layout style={{
-      overflowX: "auto"
-    }}>
-
-      <Sider className="sider" breakpoint="md"
+    <Layout
+      style={{
+        overflowX: "auto",
+      }}
+    >
+      <Sider
+        className="sider"
+        breakpoint="md"
         collapsedWidth="0"
-        style={{ background: "#fff" }}>
+        style={{ background: "#fff" }}
+      >
         <div className="logo">
           <img width="180px" height="150px" src="/cvr.jpg" alt="" />
         </div>
         <Divider></Divider>
         <NavigationMenu />
       </Sider>
-      <Layout
-        style={{ minHeight: "100%" }} >
+      <Layout style={{ minHeight: "100%" }}>
         {/* <NavigationHeader /> */}
-        <div ref={setContainer}>
-          <Affix target={() => container}>
-            <Filter></Filter>
-          </Affix>
-        </div>
+        <Filter></Filter>
 
         <Content
           style={{
@@ -42,19 +40,41 @@ const DashBoard = () => {
             margin: 0,
             minHeight: 280,
             height: "100%",
-            paddingTop: "1.5rem"
+            paddingTop: "1.5rem",
+            overFlow: "auto",
           }}
         >
           <Route exact path="/">
             <Redirect to="oxygen"></Redirect>
           </Route>
-          <Route path="/oxygen" render={(props) => <ResourceGrid id="oxygen" {...props} />}></Route>
-          <Route path="/beds" render={(props) => <ResourceGrid id="beds" {...props} />}></Route>
-          <Route path="/remdesivir" render={(props) => <ResourceGrid id="remdesivir" {...props} />}></Route>
-          <Route path="/fabiflu" render={(props) => <ResourceGrid id="fabiflu" {...props} />}></Route>
-          <Route path="/plasma" render={(props) => <ResourceGrid id="plasma" {...props} />}></Route>
-          <Route path="/others" render={(props) => <ResourceGrid id="others" {...props} />}></Route>
-          <Route path="/links" render={(props) => <ResourceList id="links" {...props} />}></Route>
+          <Route
+            path="/oxygen"
+            render={(props) => <ResourceGrid id="oxygen" {...props} />}
+          ></Route>
+          <Route
+            path="/beds"
+            render={(props) => <ResourceGrid id="beds" {...props} />}
+          ></Route>
+          <Route
+            path="/remdesivir"
+            render={(props) => <ResourceGrid id="remdesivir" {...props} />}
+          ></Route>
+          <Route
+            path="/fabiflu"
+            render={(props) => <ResourceGrid id="fabiflu" {...props} />}
+          ></Route>
+          <Route
+            path="/plasma"
+            render={(props) => <ResourceGrid id="plasma" {...props} />}
+          ></Route>
+          <Route
+            path="/others"
+            render={(props) => <ResourceGrid id="others" {...props} />}
+          ></Route>
+          <Route
+            path="/links"
+            render={(props) => <ResourceList id="links" {...props} />}
+          ></Route>
         </Content>
       </Layout>
     </Layout>
